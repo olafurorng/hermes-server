257a9f37db539d9a1f874d09703e0205afaaeecc
Ólafur Örn <olafur@plainvanillagames.com>
2015-09-30 02:26:36 +0000

    Updating /login endpoint serverside, so if user doesn't exist, we create one in database, else we return data from database

---
+++ src/main/java/misc/Log.java
@@ -0,0 +1,61 @@
+package misc;
+
+/**
+ * Created by olafurorn on 9/29/15.
+ *
+ * Wrapper for logging
+ */
+public class Log
+{
+    /**
+     * Information logging
+     */
+    public static void i(final String LOGTAG, final String msg)
+    {
+        System.out.println("info - " + LOGTAG + " : " + msg);
+    }
+
+    /**
+     * Debug logging
+     */
+    public static void d(final String LOGTAG, final String msg)
+    {
+        System.out.println("debug - " + LOGTAG + " : " + msg);
+    }
+
+    /**
+     * Error logging
+     */
+    public static void e(final String LOGTAG, final Exception e)
+    {
+        Log.e(LOGTAG, "no-message", e);
+    }
+
+    /**
+     * Error logging
+     */
+    public static void e(final String LOGTAG, final String msg)
+    {
+        Log.e(LOGTAG, msg, null);
+    }
+
+    /**
+     * Error logging
+     */
+    public static void e(final String LOGTAG, final String msg, final Exception e)
+    {
+        System.out.print("ERROR - " + LOGTAG + " : " + msg);
+
+        if (e != null)
+        {
+            System.out.println(" - STACKTRACE: ");
+            e.printStackTrace();
+        }
+        else
+        {
+            System.out.println();
+        }
+    }
+
+
+}
--- src/main/java/project/responseentities/subentities/User.java
+++ src/main/java/project/responseentities/subentities/User.java
@@ -11,16 +11,28 @@ import project.serverentities.FullUser;
  */
 public class User
 {
+    private final String id;
     private final String name;
+    private final String email;
 
-    public User(String name)
+    public User(String id, String name, String email)
     {
+        this.id = id;
         this.name = name;
+        this.email = email;
     }
 
     public String getName() {
         return name;
     }
 
+    public String getId()
+    {
+        return id;
+    }
 
+    public String getEmail()
+    {
+        return email;
+    }
 }
